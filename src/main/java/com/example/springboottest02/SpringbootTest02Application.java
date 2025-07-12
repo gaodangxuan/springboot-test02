67b25cf3a54ab02adb1e15423d071a4f24176463
GDX <13239103294@163.com>
2025-07-12 16:30:03 +0800

    Initial commit

---
+++ src/main/java/com/example/springboottest02/SpringbootTest02Application.java
@@ -0,0 +1,13 @@
+package com.example.springboottest02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTest02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottest02/SpringbootTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTest02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
